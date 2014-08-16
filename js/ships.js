f9ea1ec20078a886ca3d3b6d5e110c00422e6ec8
dlsso <supertonesfan@hotmail.com>
2014-08-16 15:15:34 -0600

    Spaceship type physics implemented

--- js/ships.js
+++ js/ships.js
@@ -44,9 +44,9 @@ var eurecaClientSetup = function() {
 		if (shipsList[id]) {
 			shipsList[id].kill();
 			delete shipsList[id];
-		    if(id === ship.id){
-			    restartButton = game.add.button(200, 200, 'restart', restart, this);
-			    restartButton.fixedToCamera = true
+			if(id === ship.id){
+				restartButton = game.add.button(200, 200, 'restart', restart, this);
+				restartButton.fixedToCamera = true
 			}
 			// console.log('killing ', id, shipsList[id]);
 		}
@@ -96,47 +96,47 @@ Ship = function (index, game, player, x, y) {
 		fire:false
 	}
 
-    // var x = Math.floor(Math.random() * 2000) + 1
-    // var x = 100
-    // var y = 100
-    // var y = Math.floor(Math.random() * 2000) + 1
+	// var x = Math.floor(Math.random() * 2000) + 1
+	// var x = 100
+	// var y = 100
+	// var y = Math.floor(Math.random() * 2000) + 1
 
 
-    this.game = game;
-    this.health = 30;
-    this.player = player;
-    this.bullets = game.add.group();
-    this.bullets.enableBody = true;
-    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    this.bullets.createMultiple(20, 'bullet', 0, false);
-    this.bullets.setAll('anchor.x', 0.5);
-    this.bullets.setAll('anchor.y', 0.5);
-    this.bullets.setAll('outOfBoundsKill', true);
-    this.bullets.setAll('checkWorldBounds', true);	
+	this.game = game;
+	this.health = 30;
+	this.player = player;
+	this.bullets = game.add.group();
+	this.bullets.enableBody = true;
+	this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+	this.bullets.createMultiple(20, 'bullet', 0, false);
+	this.bullets.setAll('anchor.x', 0.5);
+	this.bullets.setAll('anchor.y', 0.5);
+	this.bullets.setAll('outOfBoundsKill', true);
+	this.bullets.setAll('checkWorldBounds', true);	
 	
 	
 	this.currentSpeed =0;
-    this.fireRate = 200;
-    this.nextFire = 0;
-    this.alive = true;
+	this.fireRate = 200;
+	this.nextFire = 0;
+	this.alive = true;
 
-    this.shadow = game.add.sprite(x, y, 'enemy', 'shadow');
-    this.ship = game.add.sprite(x, y, 'enemy', 'ship1');
-    this.turret = game.add.sprite(x, y, 'enemy', 'turret');
+	this.shadow = game.add.sprite(x, y, 'enemy', 'shadow');
+	this.ship = game.add.sprite(x, y, 'enemy', 'ship1');
+	this.turret = game.add.sprite(x, y, 'enemy', 'turret');
 
-    this.shadow.anchor.set(0.5);
-    this.ship.anchor.set(0.5);
-    this.turret.anchor.set(0.3, 0.5);
+	this.shadow.anchor.set(0.5);
+	this.ship.anchor.set(0.5);
+	this.turret.anchor.set(0.3, 0.5);
 
-    this.ship.id = index;
-    game.physics.enable(this.ship, Phaser.Physics.ARCADE);
-    this.ship.body.immovable = false;
-    this.ship.body.collideWorldBounds = true;
-    this.ship.body.bounce.setTo(0, 0);
+	this.ship.id = index;
+	game.physics.enable(this.ship, Phaser.Physics.ARCADE);
+	this.ship.body.immovable = false;
+	this.ship.body.collideWorldBounds = true;
+	this.ship.body.bounce.setTo(0, 0);
 
-    this.ship.angle = 0;
+	this.ship.angle = 0;
 
-    game.physics.arcade.velocityFromRotation(this.ship.rotation, 0, this.ship.body.velocity);
+	game.physics.arcade.velocityFromRotation(this.ship.rotation, 0, this.ship.body.velocity);
 
 };
 
@@ -175,66 +175,67 @@ Ship.prototype.update = function() {
 	//cursor value is now updated by eurecaClient.exports.updateState method
 	
 
-    if (this.cursor.left)
-    {
-        this.ship.angle -= 4;
-    }
-    else if (this.cursor.right)
-    {
-        this.ship.angle += 4;
-    }	
-    if (this.cursor.up)
-    {
-        //  The speed we'll travel at
-        this.currentSpeed = 300;
-    }
-    else
-    {
-        if (this.currentSpeed > 0)
-        {
-            this.currentSpeed -= 4;
-        }
-    }
-    if (this.cursor.fire)
-    {	
-		this.fire({x:this.cursor.tx, y:this.cursor.ty});
-    }
-	
-	
-	
-    if (this.currentSpeed > 0)
-    {
-        game.physics.arcade.velocityFromRotation(this.ship.rotation, this.currentSpeed, this.ship.body.velocity);
-    }	
+	if (this.cursor.left)
+	{
+		this.ship.angle -= 4;
+	}
+	else if (this.cursor.right)
+	{
+		this.ship.angle += 4;
+	}	
+	if (this.cursor.up)
+	{
+		//  The speed we'll travel at
+		this.ship.body.velocity.x += Math.cos(this.ship.rotation)*10
+		this.ship.body.velocity.y += Math.sin(this.ship.rotation)*10
+
+	}
 	else
 	{
-		game.physics.arcade.velocityFromRotation(this.ship.rotation, 0, this.ship.body.velocity);
+		if (this.currentSpeed > 0)
+		{
+			this.currentSpeed -= 2;
+		}
+	}
+	if (this.cursor.fire)
+	{	
+		this.fire({x:this.cursor.tx, y:this.cursor.ty});
+	}
+	// The *.4 creates a parallax scrolling effect
+	land.tilePosition.x = -game.camera.x*.4;
+	land.tilePosition.y = -game.camera.y*.4;	
+
+	if(this.cursor.up) slideDirection = this.ship.rotation
+	
+	if (this.currentSpeed > 0)
+	{
+		game.physics.arcade.velocityFromRotation(slideDirection, this.currentSpeed, this.ship.body.velocity);
 	}
 	
 	
-    this.shadow.x = this.ship.x;
-    this.shadow.y = this.ship.y;
-    this.shadow.rotation = this.ship.rotation;
-    this.turret.rotation = this.ship.rotation;
+	this.shadow.x = this.ship.x;
+	this.shadow.y = this.ship.y;
+	this.shadow.rotation = this.ship.rotation;
+	this.turret.rotation = this.ship.rotation;
 
-    this.turret.x = this.ship.x;
-    this.turret.y = this.ship.y;
+	this.turret.x = this.ship.x;
+	this.turret.y = this.ship.y;
 };
 
 
 Ship.prototype.fire = function(target) {
 		if (!this.alive) return;
-        if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
-        {
-            this.nextFire = this.game.time.now + this.fireRate;
-            var bullet = this.bullets.getFirstDead();
-            // Using sin and cos to add offset in direction tank is facing
-            bullet.reset(this.turret.x + Math.cos(this.turret.rotation)*50, this.turret.y + Math.sin(this.turret.rotation)*50);
+		if (this.game.time.now > this.nextFire && this.bullets.countDead() > 0)
+		{
+			this.nextFire = this.game.time.now + this.fireRate;
+			var bullet = this.bullets.getFirstDead();
+			// Using sin and cos to add offset in direction tank is facing
+			bullet.reset(this.turret.x + Math.cos(this.turret.rotation)*50, this.turret.y + Math.sin(this.turret.rotation)*50);
 
 
 			bullet.rotation = this.turret.rotation;
 			game.physics.arcade.velocityFromRotation(this.turret.rotation, 800, bullet.body.velocity);
-        }
+		}
 }
 
 
@@ -249,27 +250,27 @@ var game = new Phaser.Game(viewportWidth, viewportHeight, Phaser.AUTO, 'phaser-e
 
 function preload () {
 
-    game.load.atlas('ship', 'assets/ships.png', 'assets/ships.json');
-    game.load.atlas('enemy', 'assets/enemy-ships.png', 'assets/ships.json');
-    game.load.image('logo', 'assets/logo.png');
-    game.load.image('bullet', 'assets/bullet.png');
-    game.load.image('earth', 'assets/scorched_earth.png');
-    game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64, 23);
-    game.load.image('restart','assets/restart.png'); 
+	game.load.atlas('ship', 'assets/ships.png', 'assets/ships.json');
+	game.load.atlas('enemy', 'assets/enemy-ships.png', 'assets/ships.json');
+	game.load.image('logo', 'assets/logo.png');
+	game.load.image('bullet', 'assets/bullet.png');
+	game.load.image('earth', 'assets/scorched_earth.png');
+	game.load.spritesheet('kaboom', 'assets/explosion.png', 64, 64, 23);
+	game.load.image('restart','assets/restart.png'); 
 }
 
 
 
 function create () {
-    //  Resize our game world to be a 2000 x 2000 square
-    game.world.setBounds(0, 0, 1000, 1000);
+	//  Resize our game world to be a 2000 x 2000 square
+	game.world.setBounds(0, 0, 1000, 1000);
 	game.stage.disableVisibilityChange  = true;
 	
-    //  Our tiled scrolling background
-    land = game.add.tileSprite(0, 0, viewportWidth, viewportHeight, 'earth');
-    land.fixedToCamera = true;
-    
-    shipsList = {};
+	//  Our tiled scrolling background
+	land = game.add.tileSprite(0, 0, viewportWidth, viewportHeight, 'earth');
+	land.fixedToCamera = true;
+	
+	shipsList = {};
 	player = new Ship(myId, game, ship);
 	shipsList[myId] = player;
 	ship = player.ship;
@@ -279,27 +280,27 @@ function create () {
 	bullets = player.bullets;
 	shadow = player.shadow;
 
-    //  Explosion pool
-    explosions = game.add.group();
-    for (var i = 0; i < 10; i++)
-    {
-        var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
-        explosionAnimation.anchor.setTo(0.5, 0.5);
-        explosionAnimation.animations.add('kaboom');
-    }
+	//  Explosion pool
+	explosions = game.add.group();
+	for (var i = 0; i < 10; i++)
+	{
+		var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
+		explosionAnimation.anchor.setTo(0.5, 0.5);
+		explosionAnimation.animations.add('kaboom');
+	}
 
-    ship.bringToTop();
-    turret.bringToTop();
+	ship.bringToTop();
+	turret.bringToTop();
 		
-    logo = game.add.sprite(0, 200, 'logo');
-    logo.fixedToCamera = true;
-    game.input.onDown.add(removeLogo, this);
+	logo = game.add.sprite(0, 200, 'logo');
+	logo.fixedToCamera = true;
+	game.input.onDown.add(removeLogo, this);
 
-    game.camera.follow(ship);
-    game.camera.focusOnXY(0, 0);
+	game.camera.follow(ship);
+	game.camera.focusOnXY(0, 0);
 
-    cursors = game.input.keyboard.createCursorKeys();
-    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	cursors = game.input.keyboard.createCursorKeys();
+	fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	
 	setTimeout(removeLogo, 1000);
 
@@ -314,15 +315,15 @@ function create () {
 }
 
 function respawn () {
-    //  Resize our game world to be a 2000 x 2000 square
-    // game.world.setBounds(0, 0, 1000, 1000);
+	//  Resize our game world to be a 2000 x 2000 square
+	// game.world.setBounds(0, 0, 1000, 1000);
 	// game.stage.disableVisibilityChange  = true;
 	
-    //  Our tiled scrolling background
-    land = game.add.tileSprite(0, 0, viewportWidth, viewportHeight, 'earth');
-    land.fixedToCamera = true;
-    
-    shipsList = {};
+	//  Our tiled scrolling background
+	land = game.add.tileSprite(0, 0, viewportWidth, viewportHeight, 'earth');
+	land.fixedToCamera = true;
+	
+	shipsList = {};
 	player = new Ship(myId, game, ship);
 	shipsList[myId] = player;
 	ship = player.ship;
@@ -331,29 +332,29 @@ function respawn () {
 	ship.y= game.world.randomY
 	// bullets = player.bullets;
 	// shadow = player.shadow;
-    //  Explosion pool
-    explosions = game.add.group();
+	//  Explosion pool
+	explosions = game.add.group();
 
-    for (var i = 0; i < 10; i++)
-    {
-        var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
-        explosionAnimation.anchor.setTo(0.5, 0.5);
-        explosionAnimation.animations.add('kaboom');
-    }
+	for (var i = 0; i < 10; i++)
+	{
+		var explosionAnimation = explosions.create(0, 0, 'kaboom', [0], false);
+		explosionAnimation.anchor.setTo(0.5, 0.5);
+		explosionAnimation.animations.add('kaboom');
+	}
 
-    ship.bringToTop();
-    turret.bringToTop();
+	ship.bringToTop();
+	turret.bringToTop();
 		
-    // logo = game.add.sprite(0, 200, 'logo');
-    // logo.fixedToCamera = true;
-    // game.input.onDown.add(removeLogo, this);
+	// logo = game.add.sprite(0, 200, 'logo');
+	// logo.fixedToCamera = true;
+	// game.input.onDown.add(removeLogo, this);
 	// setTimeout(removeLogo, 1000);
 
-    game.camera.follow(ship);
-    game.camera.focusOnXY(0, 0);
+	game.camera.follow(ship);
+	game.camera.focusOnXY(0, 0);
 
-    // cursors = game.input.keyboard.createCursorKeys();
-    // fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	// cursors = game.input.keyboard.createCursorKeys();
+	// fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 	
 
 	var keys = {
@@ -367,8 +368,8 @@ function respawn () {
 }
 
 function removeLogo () {
-    game.input.onDown.remove(removeLogo, this);
-    logo.kill();
+	game.input.onDown.remove(removeLogo, this);
+	logo.kill();
 }
 
 function update () {
@@ -382,12 +383,8 @@ function update () {
 	player.input.tx = game.input.x+ game.camera.x;
 	player.input.ty = game.input.y+ game.camera.y;
 	
-	turret.rotation = ship.rotation
-    land.tilePosition.x = -game.camera.x;
-    land.tilePosition.y = -game.camera.y;
-	
-    for (var i in shipsList)
-    {
+	for (var i in shipsList)
+	{
 		if (!shipsList[i]) continue;
 		var curBullets = shipsList[i].bullets;
 		var curShip = shipsList[i].ship;
@@ -409,29 +406,29 @@ function update () {
 				// shipsList[j].update();
 			}			
 		}
-    }
+	}
 }
 
 function bulletHitPlayer (ship, bullet) {
-    bullet.kill();
-    shipsList[ship.id].health -= 10
-    if (shipsList[ship.id].health<=0)
-    {
-        var explosionAnimation = explosions.getFirstExists(false);
-        explosionAnimation.reset(ship.x, ship.y);
-        explosionAnimation.play('kaboom', 30, false, true);
-    	setTimeout(function(){eurecaServer.deletePlayer(ship.id)},40)
-    }
+	bullet.kill();
+	shipsList[ship.id].health -= 10
+	if (shipsList[ship.id].health<=0)
+	{
+		var explosionAnimation = explosions.getFirstExists(false);
+		explosionAnimation.reset(ship.x, ship.y);
+		explosionAnimation.play('kaboom', 30, false, true);
+		setTimeout(function(){eurecaServer.deletePlayer(ship.id)},40)
+	}
 
 }
 
 function shipsCollide (ship, curShip) {
-    setTimeout(function(){
+	setTimeout(function(){
 		eurecaServer.deletePlayer(ship.id)
-        var explosionAnimation = explosions.getFirstExists(false);
-        explosionAnimation.reset(ship.x, ship.y);
-        explosionAnimation.play('kaboom', 30, false, true);
-    },40)
+		var explosionAnimation = explosions.getFirstExists(false);
+		explosionAnimation.reset(ship.x, ship.y);
+		explosionAnimation.play('kaboom', 30, false, true);
+	},40)
 }
 
 function restart () {
